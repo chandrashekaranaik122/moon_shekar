161cb872511a01ebf3fb4b5dc83f0b2e04758fcb
chandrashekaranaik122 <110366329+chandrashekaranaik122@users.noreply.github.com>
2022-08-04 18:45:37 +0530

    Add files via upload

---
+++ local file .py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[11]:
+
+
+df= pd.read_csv("https://raw.githubusercontent.com/shivam229/dsrscientist-DSData/master/Concrete_Data.csv")
+
+
+# In[12]:
+
+
+df
+
+
+# In[ ]:
+
+
+
+
